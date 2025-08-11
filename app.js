bbd1fb21fcf77e59e953866eaef4a5e665cab317
mohit <gangwar3537@gmail.com>
2025-08-11 21:03:57 +0530

    server setup done

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+
+const PORT  = process.env.PORT || 4000;
+app.listen(PORT , ()=>{
+    console.log(`app is running on port: ${PORT}.`);
+})
